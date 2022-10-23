12474ba6037d72f04155fc31f929b30692a8779b
EC2 Default User <ec2-user@ip-172-31-44-253.ap-northeast-1.compute.internal>
2022-10-23 10:57:09 +0000

    [Add]管理側問題編集・削除機能

--- app/controllers/admin/quizzes_controller.rb
+++ app/controllers/admin/quizzes_controller.rb
@@ -8,12 +8,28 @@ class Admin::QuizzesController < ApplicationController
     @item = Item.find(params[:item_id])
     @quiz = Quiz.new(quiz_params)
     @quiz.item_id = @item.id
-    binding.pry
     @quiz.save
     redirect_to item_path(@item.id)
   end
 
   def edit
+    @item = Item.find(params[:item_id])
+    @quiz = Quiz.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:item_id])
+    @quiz = Quiz.find(params[:id])
+    @quiz.item_id = @item.id
+    @quiz.update(quiz_params)
+    redirect_to item_path(@item.id)
+  end
+
+  def destroy
+    @item = Item.find(params[:item_id])
+    @quiz = Quiz.find(params[:id])
+    @quiz.destroy
+    redirect_to item_path(@item.id)
   end
 
   private
