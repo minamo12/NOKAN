73c38dbbc12c487b061c63c800c04e3ead0e3837
EC2 Default User <ec2-user@ip-172-31-44-253.ap-northeast-1.compute.internal>
2023-05-22 06:16:39 +0000

    [Add]初期データ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,13 +21,83 @@ Customer.create!(
 large_categories = LargeCategory.create!(
   [
     {name: "共通"},
-    {name: "農業"}
+    {name: "農業"},
+    {name: "共通"},
+    {name: "共通"},
+    {name: "共通"},
+    {name: "共通"},
+    {name: "共通"},
+    {name: "共通"}
   ]
-)
+  )
 
-meium_categories = MediumCategory.create!(
-	[
-	  {name: "野菜", large_category_id: large_categories[0].id},
-    {name: "果樹", large_category_id: large_categories[1].id}
-	]
-)
+medium_categories = MediumCategory.create!(
+  [
+    {name: "種子・植物体", large_category_id: large_categories[0].id},
+    {name: "栽培用具・機器具", large_category_id: large_categories[0].id},
+    {name: "環境調査", large_category_id: large_categories[0].id},
+    {name: "土・肥料", large_category_id: large_categories[0].id}
+  ]
+  )
+
+small_categories = SmallCategory.create!(
+  [
+    {name: "種子・植物体", medium_category_id: medium_categories[0].id},
+  ]
+  )
+
+items = Item.create!(
+  [
+    {
+      name: "イネ",
+      reading: "イネ",
+      information: "イネ科",
+      commentary: "",
+      large_category_id: large_categories[0].id,
+      medium_category_id: medium_categories[0].id,
+      small_category_id: small_categories[0].id
+    },
+
+    {
+      name: "トウモロコシ",
+      reading: "トウモロコシ",
+      information: "イネ科",
+      commentary: "",
+      large_category_id: large_categories[0].id,
+      medium_category_id: medium_categories[0].id,
+      small_category_id: small_categories[0].id
+    },
+
+    {
+      name: "ダイズ",
+      reading: "ダイズ",
+      information: "マメ科",
+      commentary: "",
+      large_category_id: large_categories[0].id,
+      medium_category_id: medium_categories[0].id,
+      small_category_id: small_categories[0].id
+    }
+  ]
+  )
+
+Quiz.create!(
+  [
+    {
+      question: "イネの科名はなんですか",
+      correct_answer: "イネ科",
+      item_id: items[0].id
+    },
+
+    {
+      question: "トウモロコシの科名はなんですか",
+      correct_answer: "イネ科",
+      item_id: items[1].id
+    },
+
+    {
+      question: "ダイズの科名はなんですか",
+      correct_answer: "マメ科",
+      item_id: items[2].id
+    }
+  ]
+  )
